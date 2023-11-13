93501b051846ddddd5c257e31a23eca81608fb17
Arun <arun.kumar@perfomatix.com>
2023-11-13 15:17:02 +0530

    UI changes - initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <navbar>
+        <div className='flex-container'>
+          <h3>WELCOME1</h3>
+          <h3>WELCOME2</h3>
+        </div>
+      </navbar>
+    </div>
+  );
+}
+
+export default App;
